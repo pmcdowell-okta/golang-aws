3578bf382d6b183fa3e734ca35a753ff289dbf4c
patrick <pmcdowell@okta.com>
2018-02-25 03:02:18 -0800

    update

---
+++ Makefile
@@ -0,0 +1,16 @@
+
+setup:
+	go get "github.com/aws/aws-lambda-go/events"
+	go get "github.com/aws/aws-lambda-go/lambda"
+
+clean:
+	rm -rf bin/*
+
+build:
+	@GOPATH=$(GOPATH) GOOS=linux GOARCH=amd64 go build -o ./bin/echo2 echo2.go
+	@GOPATH=$(GOPATH) GOOS=linux GOARCH=amd64 go build -o ./bin/main main.go
+
+deploy:
+	sls deploy
+
+
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"encoding/json"
+)
+
+
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	fmt.Println("Received body: ", request.Body)
+
+	datas := make(map[string]string)
+
+	datas["Dude where is my on main"]=request.Body
+
+	returnString, err := json.Marshal(datas)
+	_=err
+
+	return events.APIGatewayProxyResponse{Body: string(returnString), StatusCode: 200}, nil
+}
+
+func main() {
+
+	lambda.Start(Handler)
+}
